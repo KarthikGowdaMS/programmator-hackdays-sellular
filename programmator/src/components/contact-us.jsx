0b92fa623c70f21e4b3e56751b4881ee93c3292b
karthikgowdams03 <mskarthikgowda03@gmail.com>
2023-03-18 20:43:47 +0530

    edited contact us

--- programmator/src/components/contact-us.jsx
+++ programmator/src/components/contact-us.jsx
@@ -6,95 +6,95 @@ class Contact extends Component {
   render() {
     return (
       <div>
-        <section className="contact">
-          <div className="container-fluid">
-            <h2 className="contact-heading">Contact Us</h2>
-            <div className="row">
-              <div className="col-lg-6">
-                <div className="address">
-                  <h4><i className="fa fa-sharp fa-solid fa-location-dot"></i>Address</h4>
-                  <div className="pset">
-                    <p>Programmator</p>
-                    <p>The National Institute of Engineering</p>
-                    <p>Mysuru</p>
-                  </div>
-                </div>
-                <div className="phone">
-                  <h4><i className="fa fa-solid fa-phone"></i>Phone</h4>
-                  <div className="pset">
-                    <div className="row">
-                      <div className="col-lg-6">
-                        <p>Navaneeth Jain S.L.</p>
-                      </div>
-                      <div className="col-lg-4">
-                        <p>+91 86748 71546</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="pset">
-                    <div className="row">
-                      <div className="col-lg-6">
-                        <p>Dinesh S</p>
-                      </div>
-                      <div className="col-lg-4">
-                        <p>+91 86468 46548</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="pset">
-                    <div className="row">
-                      <div className="col-lg-6">
-                        <p>Kartik Gowda M S</p>
-                      </div>
-                      <div className="col-lg-4">
-                        <p>+91 87646 14684</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="email">
-                  <h4><i className="fa fa-solid fa-envelope"></i>Email</h4>
-                  <div className="pset">
-                    <p>navaneethjainsl@gmail.com</p>
-                  </div>
-                  <div className="pset">
-                    <p>karthikgowdams27@gmail.com</p>
-                  </div>
-                  <div className="pset">
-                    <p>dineshsarvan2114@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6 forms">
-                <h3>We'd love to hear from you!</h3>
-                <form>
-                  <div className="form-row">
-                    <div className="form-group col-md-6">
-                      <label for="email">Email</label>
-                      <input type="email" className="form-control" id="email" />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <label for="subject">Subject</label>
-                    <input type="text" className="form-control" id="subject" />
-                  </div>
-                  <div className="form-group">
-                    <label for="message">Message</label>
-                    <input type="text" className="form-control" id="message" />
-                  </div>
-                  <input type="button" value="Send Mail" id="EmailBtn" onclick="sendMail()" />
-                </form>
-              </div>
-            </div>
-
-            <div className="social">
-              <a href="https://www.instagram.com/navaneethjains.l/"><i className="fa fa-2x fa-brands fa-instagram"></i></a>
-              <a href="https://twitter.com/navaneethjainsl"><i className="fa fa-2x fa-brands fa-linkedin-in"></i></a>
-              <a href="https://www.youtube.com"><i className="fa fa-2x fa-brands fa-youtube"></i></a>
-              <a href="https://github.com/navaneethjainsl"><i className="fa fa-2x fa-brands fa-github large"></i></a>
-            </div>
-          </div>
-        </section>
+        <section id="contact">
+			<div class="container-fluid">
+			  <h2 class="contact-heading">Contact Us</h2>
+			  <div class="row">
+				<div class="col-lg-6">
+				  <div class="address">
+					<h4><i class="fa fa-sharp fa-solid fa-location-dot" aria-hidden="true"></i>Address</h4>
+					<div class="pset">
+					  <p>Programmator</p>
+					  <p>The National Institute of Engineering</p>
+					  <p>Mysuru</p>
+					</div>
+				  </div>
+				  <div class="phone">
+					<h4><i class="fa fa-solid fa-phone" aria-hidden="true"></i>Phone</h4>
+					<div class="pset">
+						<div class="row">
+							<div class="col-lg-6">
+								<p>Navaneeth Jain S.L.</p>
+							</div>
+							<div class="col-lg-4">
+								<p>+91 86748 71546</p>
+							</div>
+						</div>
+					</div>
+					<div class="pset">
+						<div class="row">
+							<div class="col-lg-6">
+								<p>Dinesh S</p>
+							</div>
+							<div class="col-lg-4">
+								<p>+91 86468 46548</p>
+							</div>
+						</div>
+					</div>
+					<div class="pset">
+						<div class="row">
+							<div class="col-lg-6">
+								<p>Kartik Gowda M S</p>
+							</div>
+							<div class="col-lg-4">
+								<p>+91 87646 14684</p>
+							</div>
+						</div>
+					</div>
+				  </div>
+				  <div class="email">
+					<h4><i class="fa fa-solid fa-envelope" aria-hidden="true"></i>Email</h4>
+					<div class="pset">
+					  <p>navaneethjainsl@gmail.com</p>
+					</div>
+					<div class="pset">
+					  <p>karthikgowdams27@gmail.com</p>
+					</div>
+					<div class="pset">
+					  <p>dineshsarvan2114@gmail.com</p>
+					</div>
+				  </div>
+				</div>
+				<div class="col-lg-6 forms">
+				  <h3>We'd love to hear from you!</h3>
+				  <form>
+					<div class="form-row">
+					  <div class="form-group col-md-6">
+						<label for="email">Email</label>
+						<input type="email" class="form-control" id="email"></input>
+					  </div>
+					</div>
+					<div class="form-group">
+					  <label for="subject">Subject</label>
+					  <input type="text" class="form-control" id="subject"></input>
+					</div>
+					<div class="form-group">
+					  <label for="message">Message</label>
+					  <input type="text" class="form-control" id="message"></input>
+					</div>
+					<input type="button" value="Send Mail" id="EmailBtn" onclick="sendMail()"></input>
+				  </form>
+				</div>
+			  </div>
+	  
+			  <div class="social">
+				<a href="https://www.instagram.com/navaneethjains.l/"><i class="fa fa-2x fa-brands fa-instagram" aria-hidden="true"></i></a>
+				<a href="https://twitter.com/navaneethjainsl"><i class="fa fa-2x fa-brands fa-linkedin-in" aria-hidden="true"></i></a>
+				<a href="https://www.youtube.com"><i class="fa fa-2x fa-brands fa-youtube" aria-hidden="true"></i></a>
+				<a href="https://github.com/navaneethjainsl"><i class="fa fa-2x fa-brands fa-github large" aria-hidden="true"></i></a>
+			  </div>
+			</div>
+		  </section>
 
 
         <center><p>&copy; 2023 Hackathon Management System</p></center>
