6736f340884b1f78295b24f26ae04e9b429da36f
karthikgowdams03 <mskarthikgowda03@gmail.com>
2023-03-18 21:23:51 +0530

    edited login

--- programmator/src/components/login.jsx
+++ programmator/src/components/login.jsx
@@ -5,56 +5,56 @@ class Login extends Component{
     state={};
     render(){
         return(
-            <div>
+            <div className="login">
 
             
     <section id="title">
-        <div class="container-fluid">
+        <div className="container-fluid">
             <h1>Login/Sign Up</h1>
         </div>
       </section>
 
     <section id="box">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-2"></div>
-                <div class="col-lg-4 sign-up">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-lg-2"></div>
+                <div className="col-lg-4 sign-up">
                     <h2>Sign Up</h2>
-                    <div class="form-group col-md-6">
+                    <div className="form-group col-md-6">
                         <label for="email">Email</label>
                         <input type="email" id="email" name="email" placeholder="E-mail"/>
                     </div>
-                    <div class="form-group col-md-6">
+                    <div className="form-group col-md-6">
                         <label for="username">Username</label>
                         <input type="text" id="username" name="username" placeholder="username"/>    
                     </div>
-                    <div class="form-group col-md-6">
+                    <div className="form-group col-md-6">
                         <label for="password">Password</label>
                         <input type="password" id="password" name="password" placeholder="Password"/>
                     </div>
-                    <div class="form-group submit">
+                    <div className="form-group submit">
                         <input type="submit" id="signUp" name="signUp_submit" value="Sign Up"/>
                     </div>
                 </div>
-                <div class="col-lg-4 log-in">
+                <div className="col-lg-4 log-in">
                     <h2>Login</h2>
-                    <div class="form-group col-md-6">
+                    <div className="form-group col-md-6">
                         <label for="email">Email</label>
                         <input type="email" id="email" name="email" placeholder="E-mail"/>
                     </div>
-                    <div class="form-group col-md-6">
+                    <div className="form-group col-md-6">
                         <label for="password">Password</label>
                         <input type="password" id="password" name="password" placeholder="Password"/>
                     </div>
-                    <div class="form-group col-md-6 hidden">
+                    <div className="form-group col-md-6 hidden">
                         <label for="password">Password</label>
                         <input type="password" id="password" name="password" placeholder="Password"/>
                     </div>
-                    <div class="form-group submit">
+                    <div className="form-group submit">
                         <input type="submit" id="login" name="login" value="Login"/>                </div>
                     </div>
                 </div>
-                <div class="col-lg-2"></div>
+                <div className="col-lg-2"></div>
         </div>
     </section>
 </div>
