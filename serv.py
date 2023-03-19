af2e694d010098a56e816a55fa6d97731b750a92
Dinesh <dineshsaravan2114@gmail.com>
2023-03-19 20:40:56 +0530

    nod

--- serv.py
+++ serv.py
@@ -1,25 +1,26 @@
 import threading
 import socket
-
 host = '127.0.0.1'
 port = 59000
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
-
 clients = []
 aliases = []
+
+
 def broadcast(message):
     for client in clients:
         client.send(message)
 
 # Function to handle clients'connections
+
+
 def handle_client(client):
     while True:
         try:
             message = client.recv(1024)
-            broadcast(message.decode('utf-8').encode('utf-8'))  # <-- decode and then encode the message
+            broadcast(message)
         except:
             index = clients.index(client)
             clients.remove(client)
@@ -35,18 +36,16 @@ def receive():
         print('Server is running and listening ...')
         client, address = server.accept()
         print(f'connection is established with {str(address)}')
-
         client.send('alias?'.encode('utf-8'))
         alias = client.recv(1024)
         aliases.append(alias)
         clients.append(client)
-        print(f'The alias of this client is {alias.decode("utf-8")}')
-
-        broadcast(f'{alias.decode("utf-8")} has connected to the chat room'.encode('utf-8'))
+        print(f'The alias of this client is {alias}'.encode('utf-8'))
+        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
         client.send('you are now connected!'.encode('utf-8'))
-
         thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
+
 
 if __name__ == "__main__":
     receive()
